46b12ab9a1db30b850907d6b29671f8a8ad5e410
Martin Honermeyer <maze@strahlungsfrei.de>
2017-01-13 01:32:33 +0100

    Allow setting outgoing http(s) proxy urls

--- searx/run.sh
+++ searx/run.sh
@@ -4,4 +4,14 @@ sed -i -e "s|base_url : False|base_url : ${BASE_URL}|g" \
        -e "s/ultrasecretkey/$(openssl rand -hex 16)/g" \
        /usr/local/searx/searx/settings.yml
 
+if [ $HTTP_PROXY_URL ]; then
+  cat >/tmp/proxies <<EOF
+    proxies:
+        http: $HTTP_PROXY_URL
+        https: $HTTPS_PROXY_URL
+EOF
+  sed -i -e "/outgoing:/r /tmp/proxies" /usr/local/searx/searx/settings.yml
+  rm /tmp/proxies
+fi
+
 exec su-exec $UID:$GID /sbin/tini -- python /usr/local/searx/searx/webapp.py
